f6b718b737211b3a274bfd645815066c41b7930f
famzyjr <famuyiwaahmed132@gmail.com>
2023-10-14 23:25:00 +0100

    codewithmosh

---
+++ codewithmosh.js
@@ -0,0 +1,125 @@
+// // // //primitive data types//
+// // // // string  let firstname =  "Ahmed";
+// // // // Number   let age      =  12;
+// // // // null      let selectedcolors = null;
+// // // // undefined  let firstname     = undefined;
+// // // // Boolean     let isApproved  = true;
+
+// // //Reference data types//
+// // // Object
+//object syntax//
+let person = {
+    name: 'Ahmed',
+    age:  30,
+} ;
+
+// Dot Notation
+// person.name = "John";
+// console.log(person.name);
+
+// // // // //Brcket Notation
+// person['age'] = 55
+// console.log(person.age);
+
+// // // // Array
+// // //  //Array syntax//
+// // // // let selectedColors = [];
+// // // // console.log(selectedColors);
+
+// // // //index of an array//
+// // // // let selectedColors = ['red', 'blue'];
+// // // // console.log(selectedColors[0]);
+
+// // // //this is used to add a new object to the array it can either be a string, number or other data types//
+// // // // let selectedColors = ['red', 'blue'];
+// // // // selectedColors[2] = 'green'
+// // // // console.log(selectedColors);
+
+
+
+
+
+// // // // function//
+//function syntax//
+
+// function greet(){
+//     console.log('hello world');
+// }
+
+// greet();
+
+
+
+// function greet(name, lastName){
+//     console.log('Hello ' + name + ' ' + lastName);
+// }
+
+// greet('John', 'smith');
+
+
+
+
+//write a function syntax//
+
+// function person () {
+
+//  console.log("Hello world");
+// }
+
+// //parameter
+// person()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // // //asssesment arrays//
+// // // // //created and array//
+// // // // let cars = ["Mazda", "Benz"];
+// // // // //added extra arrrys//
+// // // // cars[2] = "Mazda-cx7",
+// // // // cars[3] =  "Benz-4matic",
+// // // // cars[4] =   "Mazda-cx9",
+// // // // cars[5] =   "Benz-Glc",
+// // // // //checked the lenght of the array
+// // // console.log(cars.length);
+
+// // //asssescment objects//
+// //  //created an object//
+// // let person = {
+// //   firstname:  "ahmed",
+// //    age:         12,
+// //    lastName:     "famuyiwa",
+// //    phoneNumber:   9025330344, 
+// // };
+
+// // // used both notations//
+// // person.age= 100000;
+// // person['firstname'] = "famzy";
+// // person.lastName= "Ekundayo";
+// // person["phoneNumber"] = 8029870012;
+// // console.log(person);
+
+//write a function syntax//
+//created an object syntax//
+// function person () {
+
+//  console.log("Hello world");
+// }
+
+// //parameter
+// person()
+
+
